989ae952a1bd5b2aab017534fd030bb177513634
Asdrubal <azdcorp@gmail.com>
2022-07-09 19:46:55 -0500

    initial commit

---
+++ src/components/ContentBlock/RightContentBlock/index.tsx
@@ -0,0 +1,43 @@
+import { Row, Col } from "antd";
+import { withTranslation } from "react-i18next";
+import { SvgIcon } from "../../../common/SvgIcon";
+import { Button } from "../../../common/Button";
+import { ContentBlockProps } from "../types";
+import { Fade } from "react-awesome-reveal";
+import {
+  RightBlockContainer,
+  Content,
+  ContentWrapper,
+  ButtonWrapper,
+} from "./styles";
+
+const RightBlock = ({
+  title,
+  content,
+  button,
+  icon,
+  t,
+  id,
+}: ContentBlockProps) => {
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id) as HTMLDivElement;
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+    ///// BANNER PIXEL HOMIES
+  };
+  return (
+    <RightBlockContainer>
+      <Fade direction="up">
+        <Row justify="center" align="middle" id={id}>
+
+          <Col lg={11} md={11} sm={12} xs={24}>
+            <SvgIcon src={icon} width="100%" height="100%" />
+          </Col>
+        </Row>
+      </Fade>
+    </RightBlockContainer>
+  );
+};
+
+export default withTranslation()(RightBlock);
